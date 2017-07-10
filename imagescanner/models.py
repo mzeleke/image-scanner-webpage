7d5d3337b89564228d77d815dbe11dbaa9a1624b
Mariamwait Zeleke <mariamawitzeleke@gmail.com>
2017-07-10 15:15:00 -0400

    added date to model

--- imagescanner/models.py
+++ imagescanner/models.py
@@ -3,5 +3,6 @@ from django.db import models
 from datetime import datetime
 
 # Create your models here.
-class date(models.Model):
-    date = models.DateTimeField(default=datetime.now, blank=True)
+class datetime():
+    date = datetime.now()
+    print (date)
